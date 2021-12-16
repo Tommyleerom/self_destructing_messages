f5301a823ba1f654bd0f6cb5d43b31dda63f1604
tommyleerom <tommyleerom@gmail.com>
2021-12-16 10:16:40 +0300

    add scaffold Message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,45 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show destroy ]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @message, notice: "Message was successfully created." }
+        format.json { render :show, status: :created, location: @message }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @message.destroy
+    respond_to do |format|
+      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    def message_params
+      params.require(:message).permit(:report, :password)
+    end
+end
