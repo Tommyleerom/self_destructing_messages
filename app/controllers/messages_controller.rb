ea8e2d0f9e103fa57589ebd157ff07a712272651
tommyleerom <tommyleerom@gmail.com>
2021-12-20 13:36:07 +0300

    Implementing message encoding and decoding with password

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -42,4 +42,5 @@ class MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:report, :password)
     end
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,33 @@
 class Message < ApplicationRecord
 
-  def decrypt
-    EncryptionService.decrypt(report)
-  end
+  before_save :crypt_message
 
-  def encrypt
-    self.report = EncryptionService.encrypt(report)
-  end
+  def encrypt text
+  text = text.to_s unless text.is_a? String
+
+  len   = ActiveSupport::MessageEncryptor.key_len
+  salt  = SecureRandom.hex len
+  key   = ActiveSupport::KeyGenerator.new(password).generate_key salt, len
+  crypt = ActiveSupport::MessageEncryptor.new key
+  encrypted_data = crypt.encrypt_and_sign text
+  "#{salt}$$#{encrypted_data}"
+end
+
+def decrypt(text, password)
+  salt, data = text.split "$$"
+
+  len   = ActiveSupport::MessageEncryptor.key_len
+  key   = ActiveSupport::KeyGenerator.new(password).generate_key salt, len
+  crypt = ActiveSupport::MessageEncryptor.new key
+  crypt.decrypt_and_verify data
+end
+
+def crypt_message
+  self.report = self.encrypt self.report
+end
+
+def decrypt_message
+  self.decrypt(self.report, password)
+end
 
 end
