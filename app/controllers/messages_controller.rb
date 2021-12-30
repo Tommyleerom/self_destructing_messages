c1549c5c9d73f0dc1a5610a064e4d4bafa344952
tommyleerom <tommyleerom@gmail.com>
2021-12-30 12:35:54 +0300

    add sidekiq and redis

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,7 +27,6 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    Message.connection
     @message.destroy
     respond_to do |format|
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
